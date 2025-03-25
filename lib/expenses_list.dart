95e8d6ff972351169000bacd6460bea36901e8b2
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-26 00:29:24 +0100

    260320250029

---
+++ lib/expenses_list.dart
@@ -0,0 +1,24 @@
+import 'package:expence_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    //💡 ListView <- use case for list of unknown
+    //length (it could be 100, 200 or even 1000 elements)
+    //especially List.builder which gain some performance boost
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (builderContext, index) => Text(
+        expenses[index].title,
+      ),
+    );
+  }
+}
