5a3128f93aa50d315766fbc8e751dcb72e1f2bfa
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-25 21:26:42 +0100

    250320252126

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expence_tracker/expenses.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    MaterialApp(
+      home: Expenses(),
+    ),
   );
 }
