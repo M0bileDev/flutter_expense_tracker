78e552475452edfd4cf75ec1bfc9a6392ffcff7a
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-26 00:03:35 +0100

    260320250003

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,21 +2,29 @@ import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
 
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+}
+
 class Expense {
-  Expense({
-    required this.title,
-    required this.amount,
-    required this.date,
-  })
-  /*
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      /*
   💡 Initializer lists, can be used to initialize 
   class properties, with values that are 
   not received as constructor function arguments
   */
-  : id = uuid.v4();
+      : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
