f839307975a321e7e68e9bd115e8378599916800
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-26 21:41:37 +0100

    260320252141

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,6 +30,15 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
       body: Column(
         children: [
           Text('chart'),
