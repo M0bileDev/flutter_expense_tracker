ebddcee90d88e6fd2a9d835420ccf114f5a499d8
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-27 16:54:54 +0100

    270320251654

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expence_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expence_tracker/models/expense.dart';
+import 'package:expence_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -35,9 +36,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (context) => Text(
-        'Dummy data',
-      ),
+      builder: (context) => NewExpense(),
     );
   }
 
