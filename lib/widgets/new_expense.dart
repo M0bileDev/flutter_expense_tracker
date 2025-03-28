3855be24e952fa7fd52d23e240c8363315f4d6c6
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-28 18:41:29 +0100

    280320251841

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -21,6 +21,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _showDatePicker() async {
     final now = DateTime.now();
@@ -98,8 +99,28 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          SizedBox(
+            height: 16,
+          ),
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(category.name.toUpperCase()),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) return;
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              Spacer(),
               TextButton(
                 onPressed: () => Navigator.pop(context),
                 child: Text('Cancel'),
