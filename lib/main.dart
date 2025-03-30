5baa2acbb32cf241c54769229466468d3ea92d16
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-30 19:49:23 +0200

    300320251949

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:expence_tracker/widgets/expenses.dart';
 
 //💡 'k' means global varaibles, especially for styles
-final kColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepOrange);
+final kColorScheme = ColorScheme.fromSeed(seedColor: Colors.white);
 
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData(
-        useMaterial3: true,
-        //💡 Easiest and recommended way to configure colors
-        //for MT, because MT then infer and apply colors to any styles
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.primaryContainer,
-          foregroundColor: kColorScheme.onPrimaryContainer,
-        ),
-      ),
+          useMaterial3: true,
+          //💡 Easiest and recommended way to configure colors
+          //for MT, because MT then infer and apply colors to any styles
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kColorScheme.onPrimaryContainer,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: EdgeInsets.all(8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith()),
       home: Expenses(),
     ),
   );
