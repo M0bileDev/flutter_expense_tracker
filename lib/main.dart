9b2a53d5e2e4f04fb8b438862cba641951561d8f
m0bileDev <damian.ogorek.1996@gmail.com>
2025-03-30 19:34:06 +0200

    300320251934

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:expence_tracker/widgets/expenses.dart';
 
+//💡 'k' means global varaibles, especially for styles
+final kColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepOrange);
+
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        //💡 Easiest and recommended way to configure colors
+        //for MT, because MT then infer and apply colors to any styles
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.primaryContainer,
+          foregroundColor: kColorScheme.onPrimaryContainer,
+        ),
+      ),
       home: Expenses(),
     ),
   );
