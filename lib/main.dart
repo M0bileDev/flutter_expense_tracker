8d1920e9615c7759f7e8dac6f69dea468835475c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-04-01 21:15:50 +0200

    010420252115

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expence_tracker/widgets/expenses.dart';
+import 'package:flutter/services.dart';
 
 //💡 'k' means global varaibles, especially for styles
 final kColorScheme = ColorScheme.fromSeed(seedColor: Colors.white);
@@ -9,49 +10,63 @@ final kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: EdgeInsets.all(16),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
-      theme: ThemeData(
-        useMaterial3: true,
-        //💡 Easiest and recommended way to configure colors
-        //for MT, because MT then infer and apply colors to any styles
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.primaryContainer,
-          foregroundColor: kColorScheme.onPrimaryContainer,
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: EdgeInsets.all(16),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
-                fontSize: 20,
-                color: kColorScheme.onSecondaryContainer,
+  //💡 Used in conjunction with setPreferredOrientations. It has to bo turned on.
+  //It has to make sure:
+  //1) The Future function from .then is executed, orientation is locked
+  //2) Then widget tree is rendered, and app is running
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //💡 prevent system from adjustment different orientations
+  //that are not define in the list
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then(
+    (fun) {
+      runApp(
+        MaterialApp(
+          darkTheme: ThemeData.dark().copyWith(
+            colorScheme: kDarkColorScheme,
+            cardTheme: CardTheme().copyWith(
+              color: kDarkColorScheme.secondaryContainer,
+              margin: EdgeInsets.all(16),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kDarkColorScheme.primaryContainer,
+                foregroundColor: kDarkColorScheme.onPrimaryContainer,
               ),
             ),
-      ),
-      //💡 default -> themeMode: ThemeMode.system,
-      home: Expenses(),
-    ),
+          ),
+          theme: ThemeData(
+            useMaterial3: true,
+            //💡 Easiest and recommended way to configure colors
+            //for MT, because MT then infer and apply colors to any styles
+            colorScheme: kColorScheme,
+            appBarTheme: AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.primaryContainer,
+              foregroundColor: kColorScheme.onPrimaryContainer,
+            ),
+            cardTheme: CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: EdgeInsets.all(16),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer,
+              ),
+            ),
+            textTheme: ThemeData().textTheme.copyWith(
+                  titleLarge: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
+                    color: kColorScheme.onSecondaryContainer,
+                  ),
+                ),
+          ),
+          //💡 default -> themeMode: ThemeMode.system,
+          home: Expenses(),
+        ),
+      );
+    },
   );
 }
